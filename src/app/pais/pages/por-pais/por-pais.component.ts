b395d72cc6fe660ee6694aa24c228f5df1c7ade4
Diego Alvarez <deito.alvarez@gmail.com>
2022-07-25 18:34:26 -0500

    [Add] - 106. Servicio para buscar países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -10,10 +11,15 @@ export class PorPaisComponent {
 
   termino: string = 'Hola Mundo';
 
-  constructor() { }
+  constructor( private paisService: PaisService ) { }
 
   buscar(){
-    console.log(this.termino);
+    console.log('Termino buscado:', this.termino);
+
+    this.paisService.buscarPais( this.termino )
+      .subscribe( resp => {
+        console.log(resp);
+      });
   }
 
 }
